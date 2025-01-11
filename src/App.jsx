f0da48b4b5921b0b9eb9d91f62874bd69be74ccd
ArjunShrivastav1 <arjunshrivastav142@gmail.com>
2025-01-11 17:28:35 +0530

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 import {InputBox} from './components'
-import useCurrencyInfo from './hooks/useCurrencyInfo'
+import useCurrencyInfo from './Hooks/useCurrencyInfo'
 
 
 function App() {
 
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState('')
   const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
-  const [convertedAmount, setConvertedAmount] = useState(0)
+  const [convertedAmount, setConvertedAmount] = useState('')
 
   const currencyInfo = useCurrencyInfo(from)
 
   const options = Object.keys(currencyInfo)
 
   const swap = () => {
+    const amount = convertedAmount;
+    const from = to;
     setFrom(to)
     setTo(from)
     setConvertedAmount(amount)
@@ -66,7 +68,7 @@ function App() {
                             amount={convertedAmount}
                             currencyOptions={options}
                             onCurrencyChange={(currency) => setTo(currency)}
-                            selectCurrency={from}
+                            selectCurrency={to}
                             amountDisable
                         />
                     </div>
--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -4,7 +4,7 @@ function useCurrencyInfo(currency){
     const [ data, SetData] = useState({})
 
     useEffect(()=>{
-        fetch(`url`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json `)
         .then((res)=> res.json())             //convert API data which is in string from to json form
         .then((res)=> SetData(res[currency]))                //take response the put in setData 
     }, [currency])                             // dependency in currency
